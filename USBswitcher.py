3885ec1d78164b4fa2c7a3b8f5cea5e418ae08b7
Xian Cong <familykoay@gmail.com>
2022-08-11 23:09:05 +0800

    Update USBswitcher.py

--- USBswitcher.py
+++ USBswitcher.py
@@ -1,23 +1,25 @@
 import streamlit as sl
 import RPi.GPIO as GPIO
+from time import sleep
+
 GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
  
 button = 14
 GPIO.setup(button, GPIO.OUT) # GPIO Assign mode
 
-def press(switch): 
-    GPIO.output(button, GPIO.HIGH)
-    if switch:
-        sl.write('Switching...')
-        GPIO.output(button, GPIO.LOW) # switch
-        press(switch)
+def press(): 
+    sl.write('Switching...')
+    GPIO.output(button, GPIO.LOW) # switch
+    sleep(1)
 
 def main():
     sl.title("USB Swithcer")
     switch = sl.button('Switch')
-    press(switch)
-    
+    while(switch):
+        press()
+        GPIO.output(button, GPIO.HIGH)
+        switch = False
+        # print(switch)
 
 if __name__ == '__main__':
     main()
-        
